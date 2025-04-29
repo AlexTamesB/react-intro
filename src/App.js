a88a7f2d2d4799a2d8a66dcaf4a0c1ccf28a0018
AlexTamesB <jtamesb@vlacademy.com>
2025-04-28 23:54:14 -0600

    Modificaciones en la clase de 2 de abril en app.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,43 @@ import './App.css';
 //#1 los componestes son funciones que retornan HTML
 //#2 receordar los parentesis  
 //#3 clases de css van con className
+//#4 referencias Js van dentro de llaves{}
+//# 5 esas llaves sustituyen el v-=bind o las {{}} de vue
+  
+function HelloWorld() {
+const nombre ='Alexander Tames';
+return (
+  <div>
+<h1> Hello {nombre}</h1>
+     </div>
+);
+}
 
 function App() {    
+
+const logotipo = logo;
+const nombre = "Hello World";
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logotipo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
+          target="_blank" 
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header>
+      </header> 
+      <HelloWorld />
     </div>
   );
+  
 }
 
 export default App;
