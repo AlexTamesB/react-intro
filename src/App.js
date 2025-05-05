8398788feeeb390d1865edfaedeb2a629460b5aa
AlexTamesB <jtamesb@vlacademy.com>
2025-05-05 00:33:31 -0600

    Resolucion de practica : Si el contador es mayor a 5

--- src/App.js
+++ src/App.js
@@ -5,64 +5,68 @@ import './App.css';
 //#3 clases de css van con className
 //#4 referencias Js van dentro de llaves{}
 //# 5 esas llaves sustituyen el v-=bind o las {{}} de vue
-  
+
 function HelloWorld() {
-const nombre ='Alexander Tames';
-return (
-  <div>
-<h1> Hello {nombre}</h1>
-     </div>
-);
-}
-
-function App() {    
-
-const logotipo = logo;
-const nombre = "Hello World";
-const mostrarImagen = true;
-//Si el contadror es menor a 5, imprimirun mensale que diga,
-//"El contador es mayor a 5"
-//si el contadro es mayor a 5, imprimir un mensaje que diga
-//" el conmtador es mayor a 5"
-
-
-
-  return ( 
-
-    <div className="App">
-      <header className="App-header">
-        {mostrarImagen && <span>Hola</span>}
-        <img src={logotipo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank" 
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> 
-      <div>
-        {/* codigo react */}
-      {mostrarImagen ?<img src={logotipo} className="app-logo" alt="logo" />
-      :'No hay imagen para mostrar'}
-      
-      
-      </div>
-      
-
-
-      <HelloWorld />
-
-      <div>{mostrarImagen? <img src={logotipo} className="App-logo" alt="logo" /> : 'No hay imagen para mostra'}
-      
-      </div>
+  const nombre = 'Alexander Tames';
+  return (
+    <div className="app app-header">
+      <p>
+        Hello {nombre}
+      </p>
     </div>
   );
-  
+}
+
+
+function App() {
+  const logotipo = logo;
+  const mostrarImagen = true;
+  const contador = 10;
+
+  // Si el contador es menor a 5, imprimir un mensaje:
+  // "El contador es menor a 5"
+  // Si el contador es mayor a 5, imprimir:
+  // "El contador es mayor a 5"
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          {mostrarImagen && <span>Hola</span>}
+          <img src={logotipo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+        <div>
+          {/* código react */}
+          {mostrarImagen ? (
+            <img src={logotipo} className="App-logo" alt="logo" />
+          ) : (
+            'No hay imagen para mostrar'
+          )}
+        </div>
+
+        <div>
+          {/* Condicional del contador */}
+          {contador < 5
+            ? 'El contador es menor a 5'
+            : 'El contador es mayor a 5'}
+        </div>
+
+        <HelloWorld />
+      </div>
+    </>
+  );
 }
 
 export default App;
